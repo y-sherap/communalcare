b99700f9c9f9c251a8b0d5bab961ca93d4543d33
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-09-04 22:16:50 -0400

    protected Request routes and tested in insomnia

--- routes/RequestRouter.js
+++ routes/RequestRouter.js
@@ -1,10 +1,20 @@
 const Router = require('express').Router()
 const controller = require('../controllers/RequestController')
+const middleware = require('../middleware')
 
 Router.get('/get_all',controller.getAllRequests)
 Router.get('/:user_id',controller.getRequestByUser)
-Router.post('/:user_id',controller.createRequest)
-Router.put('/:request_id',controller.updateRequest)
-Router.delete('/:request_id',controller.deleteRequest)
+Router.post('/:user_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.createRequest)
+Router.put('/:request_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.updateRequest)
+Router.delete('/:request_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.deleteRequest)
 
 module.exports = Router
